18ca8b3f78bc5d5039731b5887c403c10d57b35b
h.bahlaouane <h.bahlaouane@void.fr>
2023-02-17 19:04:00 +0100

    feat(snack): add   '@vactory-native/ui',

--- snackager/src/bundler/externals.ts
+++ snackager/src/bundler/externals.ts
@@ -34,6 +34,7 @@ const CORE_EXTERNALS = [
   'react-native-web/dist/modules/UnimplementedView', // Used by react-native-maps
   'react-native/Libraries/Utilities/codegenNativeCommands', // Used by react-native-webview@11.23.0
   '@vactory/ui',
+  '@vactory-native/ui',
   // TODO: decide whether to treat prop-types as an external or not
   // previously it was always installed as a dependency and not treated as an external.
   // This however caused packages to be slightly larger than needed to be.
