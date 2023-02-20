ce3b9827b524f03fc60d8bb9ef16c550a280250c
h.bahlaouane <h.bahlaouane@void.fr>
2023-02-20 09:44:51 +0100

    fix(snack-sdk): example & bundler

--- packages/snack-sdk/example/next.config.js
+++ packages/snack-sdk/example/next.config.js
@@ -4,6 +4,12 @@ module.exports = {
   reactStrictMode: true,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.alias['snack-sdk'] = path.resolve(__dirname, '../../../packages/snack-sdk');
+    config.resolve.alias['vm2'] = false;
     return config;
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
